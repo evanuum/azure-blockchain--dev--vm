eb53136e7efa9099b5ce13b3b2f6ec290c222ebe
evanuum <evanuum@users.noreply.github.com>
2017-06-23 07:41:47 +0100

    Update SetupDevVM.ps1

--- scripts/SetupDevVM.ps1
+++ scripts/SetupDevVM.ps1
@@ -8,25 +8,25 @@ $secPassword = ConvertTo-SecureString $passwd -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($admin)", $secPassword)
 
 #Enable remoting
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
+Enable-PSRemoting -Force
 
 # Install Choco
 $sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
-Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential 
+Invoke-Command -ScriptBlock $sb 
 
 #"Install each Chocolatey Package"
 $chocoPackages.Split(";") | ForEach-Object {
-    $command = "choco install " + $_ + " -y -force"
+    $command = "choco install " + $_ + " -y"
     $sb = [scriptblock]::Create("$command")
 
     # Use the current user profile
-    Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential 
+    Invoke-Command -ScriptBlock $sb
 }
 
 # refresh env
 $command = "RefreshEnv"
 $sb = [scriptblock]::Create("$command")
-Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential 
+Invoke-Command -ScriptBlock $sb
 
 # Install npm packages
 $command = "npm install -g npm"
